05e3e6e8ee2370830259ba48e277a3586b3a98b3
JavierLlanos6 <xavi6poop@gmail.com>
2023-09-07 19:43:38 -0400

    V1.1 Added page structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+import Select from 'react-select';
+
+const options = [
+  {value: 'all', label: 'All'},
+  {value: 'music', label: 'Music'},
+  {value: 'tvShow', label: 'TvShow'},
+]
 
 function App() {
   return (
     <div className="App">
-      
-        <p>
-          created new branch
-        </p>
-       
+      <h1>Search your artist!</h1>
+      <input 
+        type='text'
+        placeholder='Name of artist'
+        value='{none}'
+        onChange='{none}'
+      />
+      <Select 
+        options = {options}
+      />  
+      <button>
+        Buscar
+      </button>     
     </div>
   );
 }
